24124e14b9bc5c181d39974583d9f368b369cd7b
Josafat Quijada <jsftqjd@gmail.com>
2022-07-13 19:51:05 -0400

    Rename and Rewrite main files of Calculator, ver1

--- js/main.js
+++ js/main.js
@@ -1,216 +1,229 @@
-const language = navigator.language.split("-")[0];
-const fecha = new Date();
+function operaciones(){
 
-document.querySelector(".show-about").addEventListener("click",()=>{
-  	document.querySelector(".about").classList.add("showed");
-});
+	const language = navigator.language.split("-")[0];
 
-document.querySelector(".close").addEventListener("click",(e)=>{
-  	e.target.parentElement.classList.remove("showed");
-});
+	document.querySelector(".about").firstElementChild.innerHTML = language == "es" ? `Todos los derechos reservados para "Ionic" y el uso de sus iconos "Ion-icons" &#169; <b class="year"></b>` : `Copyright "Ionic" and the use of its icons "Ion-icons" &#169; <b class="year"></b>`;
+	document.querySelector(".year").innerHTML = new Date().getFullYear();
 
-document.querySelector(".delete").textContent = language == "es" ? "Borrar todo" : "Delete all";
+	if (document.body.clientWidth <= 460) {
+		document.querySelector(".darkMode").setAttribute("style",`width:${document.querySelectorAll(".buttons")[0].clientWidth}px !important;height:${document.querySelectorAll(".buttons")[0].clientHeight}px !important;`);
+		document.querySelector(".about-button").setAttribute("style",`width:${document.querySelectorAll(".buttons")[0].clientWidth}px !important;height:${document.querySelectorAll(".buttons")[0].clientHeight}px !important;`);
+	}
+	else {
+		document.querySelector(".about-button").removeAttribute("style");
+		document.querySelector(".darkMode").removeAttribute("style");
+	}
 
-document.querySelector(".not-have").textContent = language == "es" ? "No hay tareas por hacer" : "No task for do";
-
-document.querySelector(".text").setAttribute("placeholder",language == "es" ? "Anote su objetivo" : "Write your task");
-
-document.querySelector(".copyright").innerHTML = language == "es" ? `Todos los derechos reservados para "Ionic" y el uso de sus iconos "Ion-icons" &#169;` : `Copyright "Ionic" and the use of its icons "Ion-icons" &#169;`;
-  
-const text = document.querySelector(".text");
-const list = document.querySelector(".list");
-
-const actualizar = ()=>{
-  	document.querySelector(".not-have").style.display = document.querySelector(".list").children[1] ? "none" : "inline-block";
-
-  	let titulos = [];
-  	let textos = [];
-  	let checks = [];
-
-  	document.querySelectorAll(".element").forEach(element=>{
-  		titulos.push(element.children[1].textContent);
-  	});
-  	document.querySelectorAll(".texto").forEach(texto=>{
-  		textos.push(texto.value);
-  	});
-  	document.querySelectorAll(".object").forEach(objeto=>{
-  		checks.push(objeto.classList.contains("ok") ? true : false);
-  	});
-
-  	localStorage.setItem("Titulos",titulos);
-  	localStorage.setItem("Textos",textos);
-  	localStorage.setItem("Checks",checks);
-};
-
-function agregar(titulo,texto="",checked="false") {
-  	let object = document.createElement("div");
-  	object.classList.add("object");
-  	if (checked == "true"){
-  		object.classList.add("ok");
-  	}
-
-  	let element = document.createElement("div");
-  	element.classList.add("element");
-
-  	let spans = [document.createElement("span"),document.createElement("span"),document.createElement("span")];
-
-  	let imgs = [document.createElement("img"),document.createElement("img"),document.createElement("img"),document.createElement("img")];
-
-  	imgs[0].setAttribute("src","images/svg/checkbox.svg");
-  	imgs[1].setAttribute("src","images/svg/chevron-down.svg");
-  	imgs[2].setAttribute("src","images/svg/trash-outline.svg");
-  	imgs[2].classList.add("deleted-one");
-  	imgs[3].setAttribute("src","images/svg/trash.svg");
-  	imgs[3].classList.add("deleted-two");
-
-  	spans[0].appendChild(imgs[0]);
-  	spans[1].appendChild(imgs[1]);
-  	spans[2].appendChild(imgs[2]);
-  	spans[2].appendChild(imgs[3]);
-
-  	spans[0].classList.add("checker");
-
-  	spans[0].addEventListener("click",e=>{
-  		e.target.parentElement.parentElement.parentElement.classList.toggle("ok");
-  		actualizar();
-  	});
-
-  	spans[1].classList.add("unfold");
-
-  	spans[1].addEventListener("click",e=>{
-  		e.target.parentElement.parentElement.parentElement.classList.toggle("unfolded");
-  	});
-
-  	spans[2].classList.add("deleted");
-
-  	spans[2].addEventListener("click",e=>{
-  		e.target.parentElement.parentElement.parentElement.parentElement.removeChild(e.target.parentElement.parentElement.parentElement);
-  		actualizar();
-  	});
-
-  	let h2 = document.createElement("h2");
-
-  	h2.addEventListener("keyup",actualizar);
-
-  	h2.setAttribute("contenteditable","");
-  	h2.textContent = titulo;
-  	h2.setAttribute("style","font-size:1.2em;outline:none;")
-
-  	element.appendChild(spans[0]);
-  	element.appendChild(h2);
-  	element.appendChild(spans[1]);
-  	element.appendChild(spans[2]);
-
-  	object.appendChild(element);
-
-  	let info = document.createElement("div");
-  	info.classList.add("info");
-  	let textArea = document.createElement("textarea");
-
-  	textArea.addEventListener("keyup",actualizar);
-
-  	textArea.classList.add("texto");
-  	textArea.textContent = texto;
-  	info.appendChild(textArea);
-
-  	object.appendChild(info);
-
-  	list.appendChild(object);
-
-  	document.querySelector(".not-have").style.display = document.querySelector(".list").children[1] ? "none" : "inline-block";
-}
-
-function retornar(titulo,texto,check){
-  	if (titulo){
-  		let checks = check.split(",");
-  		let titulos = titulo.split(",");
-  		let textos = texto.split(",");
-  		for (let o=0; o < titulos.length;o++){
-  			agregar(titulos[o],textos[o],checks[o]);
-  		}
-  	}
-}
-
-retornar (localStorage.getItem("Titulos"),localStorage.getItem("Textos"),localStorage.getItem("Checks"));
-
-function mesdia(opcion,mesdia){
-  	if (opcion == 1){
-  		switch (mesdia) {
-			case 0 :
-				return language == "es" ? "Enero" : "January";
-			case 1 :
-				return language == "es" ? "Febrero" : "February";
-			case 2 :
-				return language == "es" ? "Marzo" : "March";
-			case 3 :
-				return language == "es" ? "Abril" : "April";
-			case 4 :
-				return language == "es" ? "Mayo" : "May";
-			case 5 :
-				return language == "es" ? "Junio" : "June";
-			case 6 :
-				return language == "es" ? "Julio" : "July";
-			case 7 :
-				return language == "es" ? "Agosto" : "August";
-			case 8 :
-				return language == "es" ? "Septiembre" : "September";
-			case 9 :
-				return language == "es" ? "Octubre" : "October";
-			case 10 :
-				return language == "es" ? "Noviembre" : "November";
-			case 11 :
-				return language == "es" ? "Diciembre" : "December";
-		}
-  	} else {
-  		switch (mesdia) {
-			case 0 :
-				return language == "es" ? "Domingo" : "Sunday";
-			case 1 :
-				return language == "es" ? "Lunes" : "Monday";
-			case 2 :
-				return language == "es" ? "Martes" : "Tuesday";
-			case 3 :
-				return language == "es" ? "Miércoles" : "Wednesday";
-			case 4 :
-				return language == "es" ? "Jueves" : "Thursday";
-			case 5 :
-				return language == "es" ? "Viernes" : "Friday";
-			case 6 :
-				return language == "es" ? "Sábado" : "Saturday"; 
-		}
-  	}
-}
-
-document.querySelector(".dia").textContent = fecha.getDate();
-document.querySelector(".mes").textContent = mesdia(1,fecha.getMonth());
-document.querySelector(".week").textContent = mesdia(2,fecha.getDay());
-document.querySelector(".año").textContent = fecha.getFullYear();
-
-window.addEventListener("load",()=>{
- 	document.querySelector(".delete").addEventListener("click",()=>{
-  		let not = document.querySelector(".not-have");
-  		list.innerHTML = "";
-  		list.appendChild(not); 
-  		actualizar();
-  		localStorage.clear();
-  	});
-
- 	document.querySelector(".add").addEventListener("click",()=>{
- 		if (text.value != "") {
- 			agregar(text.value);
- 			actualizar();
- 			text.value = "";
- 		}
- 	});
-
- 	text.addEventListener("keypress",(e)=>{
- 		if (text.value != ""){
- 			if (e.keyCode === 13 && !e.shiftKey) {
-       			e.preventDefault();
-       			agregar(text.value);
-       			actualizar();
-       			text.value = "";
-    		}
- 		} 
+	document.querySelector(".about-button").addEventListener("click",()=>{
+		document.querySelector(".about").classList.toggle("showed");
 	});
+
+	document.querySelector(".darkMode").addEventListener("click",()=>{
+		document.querySelector(".body").classList.toggle("dark-mode");
+		localStorage.setItem("DarkMode",localStorage.getItem("DarkMode") == "false" ? "true" : "false");
+	});
+
+	let operation = 
+	{
+		parenthesis: true,
+		simbol:false,
+		decimal:true,
+		exponent:false,
+		numberVia:true,
+		sqcb:false
+	};
+	let aux = true;
+	let lastText = "";
+	let text = document.getElementById("text");
+	let buttons = document.querySelectorAll(".buttons");
+
+function result(){
+	operation["exponent"] = false;
+		let valor1 = "";
+		let regex = /[⁰¹ᒾ³⁴⁵⁶⁷⁸⁹]/i;
+		let regex2 = /[+-/*÷]/i;
+		let array = ["⁰","¹","ᒾ","³","⁴","⁵","⁶","⁷","⁸","⁹"]
+		for (let i=0; i < text.value.length; i++){
+			valor1 += text.value[i] == "÷" ? "/" : 
+			(text.value[i+1] == "√" && text.value[i] == "³") ? "Math.cbrt" : 
+			(text.value[i] == "√" && text.value[i-1] != "³") ? "Math.sqrt" :
+			((text.value[i] == "√" && text.value[i-1] == "³") || (text.value[i] == "-" && text.value[i-1] == "(")) ? "" : 
+			(text.value[i].search(regex) == 0 && text.value[i-1].search(regex) == -1) ? `**${array.indexOf(text.value[i])}` :
+			(text.value[i].search(regex) == 0 && text.value[i-1].search(regex) == 0) ? `${array.indexOf(text.value[i])}` :
+			(text.value[i] == "(" && text.value[i+1] == "-") ? "(-1*" :
+			(text.value[i].search(regex2) == -1 && (text.value[i+1] == "(" || text.value[i+1] == "√")) ? `${text.value[i]}*` :
+			text.value[i]; 
+		}
+		console.log(valor1);
+		try {
+			valor1 = eval(valor1);
+		} catch (e) {
+			let error = e.toString();
+			if (error.includes("missing ) after argument list")){
+				valor1+=")";
+				valor1 = eval(valor1);
+			}
+		}
+		console.log(valor1);
+		lastText = "";
+		text.value = valor1.toString() == "Infinity" ? "Not calculable" : valor1;
+
+		valor1 = text.value;
+
+		try {
+			if (text.value.split(".")[1]){
+				let Ei = text.value.split(".")[1];
+				Ei = Ei.includes("e") ? Ei.split("e")[0].slice(0,3) + "e" + Ei.split("e")[1] : Ei.slice(0,3);
+				text.value = text.value.indexOf(".") ? text.value.split(".")[0] + "." + Ei : text.value;	
+				valor1 = text.value;
+			}
+		} catch (e) {}			
+}
+
+function number(num){
+	let array = ["⁰","¹","ᒾ","³","⁴","⁵","⁶","⁷","⁸","⁹"];
+
+	if (operation["numberVia"] == true) {
+		if(operation["exponent"] == true){
+			operation["decimal"] = false;
+			text.value += array[num];
+		} else {
+			text.value += num;
+		}
+	operation["simbol"] = true;
+	lastText = "";
+	}
+}
+
+function simbol(sim){
+	if(operation["simbol"] == true){
+		text.value += sim;
+		lastText = "";
+		operation["simbol"] = false;
+		operation["decimal"] = true;
+		operation["exponent"] = false;
+		operation["numberVia"] = true;
+	}
+}
+
+function guardar(){
+	buttons.forEach(button=>{
+		button.addEventListener("click",()=>{
+			if (button.textContent == "√"){
+				if (operation["sqcb"] == false){
+					operation["sqcb"] = true;
+					operation["parenthesis"] = false;
+					text.value += "√(";
+				} else {
+					text.value += ")";
+					operation["sqcb"] = false;
+					operation["exponent"] = false;
+					operation["numberVia"] = false;
+				}
+			} else if (button.textContent == "³√"){
+				if (operation["sqcb"] == false){
+					operation["sqcb"] = true;
+					operation["parenthesis"] = false;
+					text.value += "³√(";
+				} else {
+					text.value += ")";
+					operation["sqcb"] = false;
+					operation["exponent"] = false;
+					operation["numberVia"] = false;
+				}
+			} else if (button.textContent == "xⁿ"){
+				if(operation["simbol"] == true){
+					if(operation["exponent"] == false){	
+						operation["decimal"] = false;
+						operation["exponent"] = true;
+					}
+				}
+			} else if (button.textContent == "÷" || button.textContent == "*" || button.textContent == "-" || button.textContent == "+"){
+				simbol(button.textContent);
+			} else if (button.textContent == "±"){
+				let array = ["⁰","¹","ᒾ","³","⁴","⁵","⁶","⁷","⁸","⁹"];
+				if (text.value.length >=1){
+					if (parseInt(text.value[text.value.length-1]) >= 0 && parseInt(text.value[text.value.length-1]) || array.includes(text.value[text.value.length-1]) <= 9){
+						if (lastText == ""){
+							let regex = /[+-/*(]/i;
+							lastText = text.value;
+							text.value = text.value.replace(text.value.split(regex)[text.value.split(regex).length-1],`(-${text.value.split(regex)[text.value.split(regex).length-1]})`);
+							operation["numberVia"] = false;
+						} else {
+							[lastText, text.value] = [text.value, lastText];
+							operation["numberVia"] = operation["numberVia"] == true ? false : true
+						}
+					}
+				}
+			} else if (parseInt(button.textContent) >= 0 && parseInt(button.textContent) <= 9){
+				number(button.textContent);
+			} else if (button.textContent == "."){
+				if(operation["decimal"] == true){
+					operation["decimal"] = false;
+					text.value += ".";
+				}
+			} else if (button.textContent == "="){
+				result();
+			} else if (button.textContent == "C"){
+				lastText = "";
+				operation["numberVia"] = true;
+				operation["exponent"] = false;
+				operation["simbol"] = false;
+				operation["decimal"] = true;
+				operation["sqcb"] = false;
+				text.value = "";
+			} else if (button.id == "BACK"){
+				text.value = text.value.substring(0,text.value.length-1);
+			} else if (button.textContent == "()") {
+				text.value += operation["parenthesis"] == true ? "(" : ")";
+				operation["sqcb"] = false;
+				operation["parenthesis"] = operation["parenthesis"] == true ? false : true;
+				operation["numberVia"] = operation["parenthesis"] == true ? false : true; 
+			} 
+
+			if (text.value != "") {
+				document.querySelector(".about-button").classList.add("d-none");
+			} else document.querySelector(".about-button").classList.remove("d-none");
+		})
+	})
+}
+
+	guardar();
+
+	window.addEventListener("keydown",(e)=>{
+		let regex = /[()+*-/.0-9]/i;
+
+		if (e.key == "Enter"){
+			result();
+		} else if (e.key == "Backspace"){
+			text.value = text.value.substring(0,text.value.length-1);
+		} else if (e.key.search(regex) == 0){
+			text.value += e.key == "/" ? "÷" : e.key;
+		}
+
+		if (text.value != "") {
+			document.querySelector(".about-button").classList.add("d-none");
+		} else document.querySelector(".about-button").classList.remove("d-none");
+	});
+}
+
+if (!localStorage.getItem("DarkMode")){
+	localStorage.setItem("DarkMode","false");
+} else {
+	if (localStorage.getItem("DarkMode") == "true"){
+		document.querySelector(".body").classList.add("dark-mode");	
+	}
+}
+
+window.addEventListener("load",operaciones);
+
+window.addEventListener("resize",()=>{
+	if (document.body.clientWidth <= 460) {
+		document.querySelector(".darkMode").setAttribute("style",`width:${document.querySelectorAll(".buttons")[0].clientWidth}px !important;height:${document.querySelectorAll(".buttons")[0].clientHeight}px !important;`);
+		document.querySelector(".about-button").setAttribute("style",`width:${document.querySelectorAll(".buttons")[0].clientWidth}px !important;height:${document.querySelectorAll(".buttons")[0].clientHeight}px !important;`);
+	}
+	else {
+		document.querySelector(".darkMode").removeAttribute("style");
+		document.querySelector(".about-button").removeAttribute("style");
+	}
 });
