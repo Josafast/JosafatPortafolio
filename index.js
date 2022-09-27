2a35bf35aa9db6cc86535315443ddb4b7f76ca6a
Josafat Quijada <jsftqjd@gmail.com>
2022-09-27 11:08:40 -0400

    Port changed

--- index.js
+++ index.js
@@ -59,4 +59,4 @@ app.get('/login',(req, res)=>{
   res.render('login');
 });
 
-app.listen(3000,()=>console.log('Server on port: 3000'));
+app.listen(process.env.PORT || 3000,()=>console.log('Server on port: ' + process.env.PORT || 3000));
